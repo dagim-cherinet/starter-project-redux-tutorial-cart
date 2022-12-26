901f8b143a01211701c57dff78c3c7c5e6f1831a
Dagim Cherinet <80765887+dagim-cherinet@users.noreply.github.com>
2022-12-26 09:58:47 +0300

    connected app to redux

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
+
 // redux stuff
+import { createStore } from "redux";
+import reducer from "./reducer";
+//react-redux  , Provider -wraps the app , connect -used in components
+import { Provider } from "react-redux";
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 5,
+};
+
+const store = createStore(reducer, initialStore);
 
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
       <CartContainer cart={cartItems} />
-    </main>
+    </Provider>
   );
 }
 
---
+++ src/reducer.js
@@ -0,0 +1,8 @@
+import { REMOVE, GET_AMOUNT, GET_TOTAL } from "./actions";
+const reducer = (state, action) => {
+  console.log({ state, action });
+
+  return state;
+};
+
+export default reducer;
